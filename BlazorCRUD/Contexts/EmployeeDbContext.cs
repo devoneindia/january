acd6c30c9d6055fb479d543624b773746e6045bb
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-01-18 19:45:28 +0530

    Added connString

---
+++ BlazorCRUD/Contexts/EmployeeDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BlazorCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUD.Contexts
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<UserDetail> UserDetails { get; set; }
+        public EmployeeDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configurationInstance = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName ?? ".")
+                .AddJsonFile("appsettings.json",optional:true)
+                .Build();
+
+            string dbConnString = configurationInstance["ConnectionStrings:DbEmployee"] ?? "";
+            optionsBuilder.UseNpgsql(dbConnString); 
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+    }
+}
